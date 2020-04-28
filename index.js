44a414a188ef91300b95cdada486517389a90979
Andres <huracan311@gmail.com>
2020-04-28 15:01:22 -0500

    dos

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express =require('express')
 const app = express();
 
 app.get('/',(req,res)=>res.send('Hello world'));
-app.listen(3000);
-console.log('Server on port',3000);
+app.listen(80);
+console.log('Server on port',80);
